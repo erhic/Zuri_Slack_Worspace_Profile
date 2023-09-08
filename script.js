5d2f7dcccc4098413dd74fae449eda0f448584ba
erhic <ericngugi24@gmail.com>
2023-09-08 18:50:04 +0300

    feat: added  current day of the week

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 function getInfo() {
   // window html-body element to display slack name
   document.getElementById("slackUserName").innerHTML += "<p>Eric Ngugi</p>";
+  currentDayOfTheWeek();
+}
+
+function currentDayOfTheWeek() {
+  const weekday = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  const d = new Date();
+  let day = weekday[d.getDay()];
+  document.getElementById("currentDayOfTheWeek").innerHTML += `<p>${day}<p>`;
 }
