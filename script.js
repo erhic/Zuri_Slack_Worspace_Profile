dc28f6b0f059866bdd04116691c6b0b7c91c1a35
erhic <ericngugi24@gmail.com>
2023-09-08 19:28:20 +0300

    feat: added link to the source code on github

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
+currentDayOfTheWeek();
+getUTCMilliseconds();
+
 function getInfo() {
   // window html-body element to display slack name
   document.getElementById("slackUserName").innerHTML += "<p>Eric Ngugi</p>";
-  currentDayOfTheWeek();
-  getUTCMilliseconds();
+
+  // window html-body element to display myTrack
+  document.getElementById("myTrack").innerHTML += "<p>Frontend</p>";
+
+  // window html-body element to link to github repo containing this source code
+  document.getElementById(
+    "githubURL"
+  ).innerHTML += `<a href="https://github.com/erhic/Zuri_Slack_Worspace_Profile">Github Repo</a>`;
 }
 
+/**
+ * Function to get the current day of the week
+ */
 function currentDayOfTheWeek() {
   const weekday = [
     "Sunday",
@@ -21,8 +33,14 @@ function currentDayOfTheWeek() {
   document.getElementById("currentDayOfTheWeek").innerHTML += `<p>${day}<p>`;
 }
 
+/**
+ * function to get the utc in milliseconds for the day
+ */
+
 function getUTCMilliseconds() {
   const d = new Date();
   let milliseconds = d.getUTCMilliseconds();
-  document.getElementById("currentUTCTime").innerHTML += `<p>$miki<p>`;
+  document.getElementById(
+    "currentUTCTime"
+  ).innerHTML += `<p>${milliseconds}<p>`;
 }
