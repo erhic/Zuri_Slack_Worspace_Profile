3b8271c82a135d26ea94d53f8da187c0c6e12310
erhic <ericngugi24@gmail.com>
2023-09-08 18:22:29 +0300

    feat: Added a DOM element to display slack name.

---
+++ script.js
@@ -0,0 +1,10 @@
+// function getInfo() {
+//   document.querySelector("slackUserName").innerHTML += "<h3>Eric Ngugi</h3>";
+//   console.log("hhd");
+// }
+// let div =document.
+
+function getInfo() {
+  // window html-body element to display slack name
+  document.getElementById("slackUserName").innerHTML += "<p>Eric Ngugi</p>";
+}
