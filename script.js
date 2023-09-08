543b9133d71373a2dde175f39115c59f6cbc6d95
erhic <ericngugi24@gmail.com>
2023-09-08 18:39:47 +0300

    feat: added  slack display  image

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-// function getInfo() {
-//   document.querySelector("slackUserName").innerHTML += "<h3>Eric Ngugi</h3>";
-//   console.log("hhd");
-// }
-// let div =document.
-
 function getInfo() {
   // window html-body element to display slack name
   document.getElementById("slackUserName").innerHTML += "<p>Eric Ngugi</p>";
